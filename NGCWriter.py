de3422b4dc479b27faa716f1e9cf1515e73e0c42
Alexander Rössler <mail@roessler.systems>
2018-12-03 17:22:18 +0100

    update for Cura 3.6

--- NGCWriter.py
+++ NGCWriter.py
@@ -1,18 +1,22 @@
 # Copyright (c) 2016-2018 Alexander Roessler
 # NGCWriter is released under the terms of the AGPLv3 or higher.
+import io  # For StringIO to write the g-code to a buffer.
+import os
+import sys
 
 from UM.Mesh.MeshWriter import MeshWriter
 from UM.Logger import Logger
 from UM.PluginRegistry import PluginRegistry
 
-import io #For StringIO to write the g-code to a buffer.
-import os
-import sys
+from UM.i18n import i18nCatalog
+
 path = os.path.dirname(os.path.realpath(__file__))
 if path not in sys.path:
     sys.path.append(path)
 from converter.gcode2ngc import GCode2Ngc
 from converter.ngc2ve import Ngc2Ve
+
+catalog = i18nCatalog("cura")
 
 
 class NGCWriter(MeshWriter):
@@ -24,6 +28,7 @@ class NGCWriter(MeshWriter):
     def write(self, stream, nodes, mode=MeshWriter.OutputMode.TextMode):
         if mode != MeshWriter.OutputMode.TextMode:
             Logger.log("e", "NGC Writer does not support non-text mode.")
+            self.setInformation(catalog.i18nc("@error:not supported", "NGCWriter does not support non-text mode."))
             return False
 
         # Get the g-code.
